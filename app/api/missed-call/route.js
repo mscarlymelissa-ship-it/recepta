0f53d1761289b4168ccd11ca15f6dfb700ec26ec
unknown <ms.carlymelissa@gmail.com>
2026-05-24 18:42:05 -0600

    hardcode supabase url to fix missed call route

--- app/api/missed-call/route.js
+++ app/api/missed-call/route.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 export async function POST(request) {
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    'https://osiouwgiaoldctuvahxb.supabase.co',
     process.env.SUPABASE_SERVICE_KEY
   )
 
